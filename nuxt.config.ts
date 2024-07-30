b7b1f549e4887bf9cb172dbb3cb50dbe97f6a6a0
nyarikikaruma <nyarikikaruma@gmail.com>
2024-07-30 21:52:01 +0300

    Fonts added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,17 @@ export default defineNuxtConfig({
   routeRules: {
     '/': { redirect: '/home' },
 },
+
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  modules: ["@nuxt/fonts"]
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ export default {
     "./error.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        custom: ['Arial'],
+      },
+    },
   },
   plugins: [],
 }
